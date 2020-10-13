fa906e468e789fdf5e87b4d5e1d9a73fb3d36277
Raj <shubham.raj.in@gmail.com>
2020-10-14 00:28:49 +0530

    injector code

---
+++ js/injector.js
@@ -0,0 +1,34 @@
+function inject_blog_data(data) {
+    var tag_string = ""
+    for(i = 0; i < data.tags.length; i++){
+        var delimiter = ","
+        if(i==data.tags.length-1){
+            delimiter = ""
+        }
+        var tag = data.tags[i]+delimiter
+        tag_string+=`<li><a href="#">${tag}</a></li>`
+    }
+    var blog_string = `<div class="single-post row">
+                                <div class="col-lg-3  col-md-3 meta-details">
+                                    <ul class="tags">
+                                        ${tag_string}
+                                    </ul>
+                                    <div class="user-details row">
+                                        <p class="user-name col-lg-12 col-md-12 col-6"><a href="#">${data.author}</a> <span class="lnr lnr-user"></span></p>
+                                        <p class="date col-lg-12 col-md-12 col-6"><a href="#">${data.date}</a> <span class="lnr lnr-calendar-full"></span></p>                     
+                                    </div>
+                                </div>
+                                
+                                <div class="col-lg-9 col-md-9 ">
+                                    <div class="feature-img">
+                                        <img class="img-fluid" src="${data.cover_image}" alt="">
+                                    </div>
+                                    <a class="posts-title" href="${data.blog_src}"><h3>${data.title}</h3></a>
+                                    <p class="excert">
+                                        ${data.summary}
+                                    </p>
+                                    <a href="${data.blog_src}" class="primary-btn">View More</a>
+                                </div>
+                            </div>`
+    return blog_string
+}
